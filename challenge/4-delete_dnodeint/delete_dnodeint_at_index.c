dd5eebc1ebb08dc1d63281a5353cf4aa3f8e31c2
Emanuell95 <emanueltute95@gmail.com>
2025-05-15 17:25:09 -0400

    Double link list0.5

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * delete_dnodeint_at_index - Delete the node at index of a dlistint_t list
+ * delete_dnodeint_at_index - Deletes the node at index of a dlistint_t list
  * @head: Double pointer to the head of the list
  * @index: Index of the node to delete
  *
@@ -10,39 +10,39 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *current;
-    unsigned int i;
+	dlistint_t *current;
+	unsigned int i;
 
-    if (head == NULL || *head == NULL)
-        return (-1);
+	if (head == NULL || *head == NULL)
+		return (-1);
 
-    current = *head;
+	current = *head;
 
-    /* Special case for first node (index 0) */
-    if (index == 0)
-    {
-        *head = current->next;
-        if (*head != NULL)
-            (*head)->prev = NULL;
-        free(current);
-        return (1);
-    }
+	/* Traverse to the node at index */
+	for (i = 0; i < index; i++)
+	{
+		if (current == NULL)
+			return (-1);
+		current = current->next;
+	}
 
-    /* Traverse to the node at the specified index */
-    for (i = 0; i < index; i++)
-    {
-        if (current->next == NULL)
-            return (-1);
-        current = current->next;
-    }
+	if (current == NULL)
+		return (-1);
 
-    /* Update the linked list pointers to bypass the node being deleted */
-    if (current->next != NULL)
-        current->next->prev = current->prev;
-    if (current->prev != NULL)
-        current->prev->next = current->next;
+	/* If the node to delete is the head */
+	if (current->prev == NULL)
+	{
+		*head = current->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+	}
+	else
+	{
+		current->prev->next = current->next;
+		if (current->next != NULL)
+			current->next->prev = current->prev;
+	}
 
-    /* Free the memory of the node being delete */
-    free(current);
-    return (1);
+	free(current);
+	return (1);
 }
